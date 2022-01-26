1123dfd2eeae83a23e42f0fc0908ab6584f3c8e9
Gonza <gonnron@gmail.com>
2022-01-26 01:20:40 -0300

    carga de contenido

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import html from "./index.html"
 import "./style.css"
 
 import nav from "./inicio.js"
-import sheader from "./stheader.js"
 
-ReactDOM.render(
-    <h1>Hola mionf </h1>,
-    document.getElementById("app")
-)
+
 
 nav();
-sheader();
